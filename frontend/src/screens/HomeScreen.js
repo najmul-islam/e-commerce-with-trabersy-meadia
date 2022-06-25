5e1b8a994e4a2afc594e80f4701f4c8020248cad
najmul-islam <najmulislam519@gmail.com>
2022-06-26 01:07:21 +0600

    add files

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product'
-import axios from 'axios';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-    return (
-        <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
-}
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-export default HomeScreen
+  return (
+    <>
+      <h1>Latest Products</h1>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message />
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
